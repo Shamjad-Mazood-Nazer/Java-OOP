34729748c7e93af9e9bfa1f8e39e76d87adaa964
Shamjad-Mazood-Nazer <97999729+Shamjad-Mazood-Nazer@users.noreply.github.com>
2022-03-29 12:44:14 +0530

    29-03-2022(Online Class)

---
+++ 29-03-2022/ExplicitTypeCasting.java
@@ -0,0 +1,9 @@
+public class ExplicitTypeCasting{
+	public static void main(String args[]){
+		float b = 4.83f;
+		int a;
+		a = (int) b; //Explicit type casting
+		System.out.println(b);
+		System.out.println(a);
+	}
+}
---
+++ 29-03-2022/TypeCasting.java
@@ -0,0 +1,9 @@
+public class TypeCasting{
+	public static void main(String args[]){
+		int a = 6;
+		float b;
+		b = a; //Automatic type casting(implicit)
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
