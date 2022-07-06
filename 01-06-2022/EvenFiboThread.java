790cbb360febf8b4523a449f73409cef74b432b5
Shamjad-Mazood-Nazer <97999729+Shamjad-Mazood-Nazer@users.noreply.github.com>
2022-07-06 15:47:06 +0530

    UPDATED CODE

--- 01-06-2022/EvenFiboThread.java
+++ 01-06-2022/EvenFiboThread.java
@@ -1,4 +1,4 @@
-import java.io.*;
+/*import java.io.*;
 import java.util.Scanner;
 
 class Fibo implements Runnable{
@@ -43,5 +43,52 @@ class EvenFiboThread{
 		Even e = new Even(n);
 		e.start();*/
 		Thread t = new Thread(t);
+	}
+}*/
+
+
+import java.util.Scanner;
+class Fibonacci extends Thread{
+	int size;
+	Fibonacci(int size){
+		this.size=size;
+	}
+	public void run(){
+		int num1=0, num2=1;
+		System.out.println("Fibonacci series : 0, 1, ");
+		for(int i=2;i<size;i++){
+			int temp=num1;
+			num1= num2;
+			num2= temp+num1;
+			System.out.println("Fibonacci series : "+num2+", ");
+		}
+	}
+}
+class EvenNumber extends Thread{
+	int range;
+	EvenNumber(int range){
+	this.range= range;
+	}
+	public void run(){
+		for(int i=0;i<range;i++){
+			if(i%2==0){
+				System.out.println("Even number are  : "+i);
+			}
+		}
+	}
+}
+public class EvenFiboThread{
+	public static void main(String[] args) {
+		int size, range;
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter the limit of fibonacci series : ");
+		size= sc.nextInt();
+		System.out.println("Enter the range of even numbers : ");
+		range= sc.nextInt();
+		Fibonacci fib= new Fibonacci(size);
+		EvenNumber even= new EvenNumber(range);
+		fib.start();
+		even.start();
+		sc.close();
 	}
 }
